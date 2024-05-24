a096a0b009149203d9f64c00228aed2f42818a4f
hexi <hexi@cambricon.com>
2024-05-24 09:04:34 +0000

    fix(triton-linalg): remove triton_linalg.cc

--- triton_linalg.cc
+++
@@ -1,8 +0,0 @@
-#include <pybind11/pybind11.h>
-
-namespace py = pybind11;
-
-// The CPU backend with triton_shared doesn't do compilation from within python
-// but rather externally through triton-shared-opt, so we leave this function
-// blank.
-void init_triton_triton_linalg(py::module &&m) {}
